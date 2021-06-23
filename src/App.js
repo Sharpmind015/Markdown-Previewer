05b1215f91edd6952375e2e50df39366cd0f4135
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-23 02:33:52 -0700

    set up split pane

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import SplitPane from "react-split-pane";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1 className="heading">Markdown Preview</h1>
+      <SplitPane split="vertical" minSize={"50%"}>
+        <div id="editor" />
+        <div id="preview" />
+      </SplitPane>
+    </div>
+  );
 }
 
 export default App;
