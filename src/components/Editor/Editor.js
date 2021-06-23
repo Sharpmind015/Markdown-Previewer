a93924a3ab2d94f29eb18bee205a7a8968220d61
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-23 02:40:36 -0700

    built editor component

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,12 @@
+const Editor = () => {
+  return (
+    <div id="editor" className="editor">
+      <label htmlFor="editor" className="editor__label">
+        Editor
+      </label>
+      <textarea name="editor" id="editor" />
+    </div>
+  );
+};
+
+export default Editor;
