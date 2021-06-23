3fb2ce8d82bc9072a9420ec78420eb62454691d1
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-23 04:40:05 -0700

    set redux for the preview component

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,13 +1,26 @@
+import { connect } from "react-redux";
+import marked from "marked";
 import "./preview.css";
 
-const Preview = () => {
+const Preview = ({ markdown }) => {
   return (
     <div id="preview" className="preview">
       <div>
-        <h1 className="preview__heading">Result</h1>
+        <h1 className="preview__heading">Results</h1>
+        <div
+          className="results"
+          dangerouslySetInnerHTML={{ __html: markdown }}
+        />
       </div>
     </div>
   );
 };
 
-export default Preview;
+const mapStateToProps = state => {
+  marked.setOptions({
+    breaks: true
+  });
+  return { markdown: marked(state.markdown) };
+};
+
+export default connect(mapStateToProps, null)(Preview);
