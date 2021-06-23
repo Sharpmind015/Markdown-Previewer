409845b9c7a0c623e844c174630c0f87f1c3d9b7
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-23 03:04:42 -0700

    remove main heading

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Preview from "./components/Preview/Preview";
 function App() {
   return (
     <div className="App">
-      <h1 className="heading">Markdown Preview</h1>
       <SplitPane split="vertical" minSize={"50%"}>
         <Editor />
         <Preview />
