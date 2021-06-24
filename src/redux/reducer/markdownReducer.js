ea67415c429da24544dd26fb93ebd3004f8febd2
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-23 19:37:12 -0700

    set initial state

--- src/redux/reducer/markdownReducer.js
+++ src/redux/reducer/markdownReducer.js
@@ -1,5 +1,33 @@
 const initialState = {
-  markdown: ""
+  markdown: `Paragraph
+**bolded text**
+    
+# heading
+## heading 2
+- list item
+    
+[google](https://google.com)
+    
+Heres some code, \`<div></div>\`, between 2 backticks.
+    
+> block quote
+    
+    
+\`\`\`
+// this is multi-line code:
+    
+function anotherExample
+(firstLine, lastLine) {
+    if (firstLine == 
+        '\`\`\`' && lastLine == '\`\`\`') {
+        return multiLineCode;
+    }
+}
+\`\`\`
+    
+![image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)
+        
+     `
 };
 const markdownReducer = (state = initialState, action) => {
   switch (action.type) {
